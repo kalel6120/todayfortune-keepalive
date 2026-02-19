61b3f759121a449b69f4be808054eddd8056da46
kalel6120 <wlstkdvlf6120@gmail.com>
2026-02-19 11:08:11 +0900

    Update visit-frontend.js

--- visit-frontend.js
+++ visit-frontend.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer');
 
 const TARGET_URLS = [
     'https://todayfor.vercel.app/',
-    'https://qrlinks.vercel.app/r/tt7prJwfjX',
-    'https://petcasso.onrender.com/gallery'
+    'https://qrlinks.vercel.app/r/tt7prJwfjX'
+
 ];
 
 async function run() {
